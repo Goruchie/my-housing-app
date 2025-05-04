d984ef463c3b0e8003506ca24e416dbb4e231839
Goruchie <marcosgualtero43253@gmail.com>
2025-05-04 01:07:52 -0300

    Implement a Form to the Details page

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -14,8 +15,22 @@ export class DetailsComponent {
   housingService: HousingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
 
+  formAngular = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  })
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
+
+  onSubmit() {
+    this.housingService.submitApplication(
+      this.formAngular.value.firstName ?? '',
+      this.formAngular.value.lastName ?? '',
+      this.formAngular.value.email ?? ''
+    );
   }
 }
